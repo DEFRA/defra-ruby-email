d38024869a715de2aeb3560984c90b151d33d4e8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-02-07 17:39:49 +0000

    Add configuration to the project

--- lib/defra_ruby_email/engine.rb
+++ lib/defra_ruby_email/engine.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "configuration"
+
 module DefraRubyEmail
   class Engine < ::Rails::Engine
     isolate_namespace DefraRubyEmail
---
+++ spec/support/helpers/configuration.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Helpers
+  module Configuration
+    def self.prep_for_tests(delay = 100)
+      DefraRubyEmail.reset_configuration
+      DefraRubyEmail.configure do |config|
+        config.enable = true
+        config.delay = delay
+      end
+    end
+
+    def self.reset_for_tests
+      DefraRubyEmail.reset_configuration
+    end
+  end
+end
