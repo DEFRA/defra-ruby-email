53802b8da5e6207658d6265ffbc8059f28b6d9a2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-02-10 16:33:28 +0000

    Implement last email controller
    Update the code to refer to a last-email controller, and render the content of the LastEmailCache when called.

--- app/controllers/defra_ruby_email/last_email_controller.rb
+++ app/controllers/defra_ruby_email/last_email_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module DefraRubyEmail
-  class TestController < ApplicationController
+  class LastEmailController < ApplicationController
     def show
-      render
+      render json: LastEmailCache.instance.last_email_json
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 DefraRubyEmail::Engine.routes.draw do
-  get "/test",
-      to: "test#show",
-      as: "test",
+  get "/last-email",
+      to: "last_email#show",
+      as: "last_email",
       constraints: ->(_request) { DefraRubyEmail.configuration.enabled? }
 end
